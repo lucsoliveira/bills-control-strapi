29d0760ea443518234f8b45c6597a67ab5fa2fec
Lucas Oliveira <olucaspereira.eng@gmail.com>
2023-09-23 14:05:16 -0300

    feat: initial files plugin page plugin

---
+++ config/plugins.ts
@@ -0,0 +1,6 @@
+export default ({ env }) => ({
+  "page-dashboard": {
+    enabled: true,
+    resolve: "./src/plugins/page-dashboard",
+  },
+});
---
+++ src/plugins/page-dashboard/README.md
@@ -0,0 +1,3 @@
+# Strapi plugin page-dashboard
+
+A quick description of page-dashboard.
